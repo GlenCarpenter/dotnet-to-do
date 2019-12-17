1e7e60aa0f7feca9f3d7cdada9f9dbc1fdd369f2
Glen Carpenter <glen@glencarpenter.io>
2019-12-17 15:45:45 -0700

    More validators and abstracted functionality

--- dotnet-todo/Program.cs
+++ dotnet-todo/Program.cs
@@ -4,18 +4,21 @@ namespace dotnet_todo
 {
     class Program
     {
+        // initializing todo list
+        static ToDoList myToDos = new ToDoList();
+        static String Input { get; set; }
+        static int selection;
+
+        // vars for current selection
+        static String NewTask { get; set; }
+        static String currentInput;
+        static int currentToDo;
+
+        // declare delegate for callbacks
+        public delegate void Callback();
+        static bool callbackExecuted = false;
         static void Main(string[] args)
         {
-            // initializing todo list
-            ToDoList myToDos = new ToDoList();
-            String input;
-            int selection;
-
-            // vars for current selection
-            String newTask;
-            String currentInput;
-            int currentToDo;
-
             Console.WriteLine("Welcome to the to-do list!\n\n");
 
             // main program
@@ -29,65 +32,51 @@ namespace dotnet_todo
                 Console.WriteLine("5. Delete a to-do item.");
                 Console.WriteLine("6. Quit.");
 
-                input = Console.ReadLine();
-                if (int.TryParse(input, out selection))
+                Input = Console.ReadLine();
+                if (int.TryParse(Input, out selection))
                 {
                     switch (selection)
                     {
                         case 1:
                             Console.WriteLine("\nEnter the new to-do item: ");
-                            newTask = Console.ReadLine();
-                            myToDos.AddToDo(newTask);
+                            NewTask = Console.ReadLine();
+                            myToDos.AddToDo(NewTask);
                             Console.WriteLine("\nNew item added.\n");
                             break;
                         case 2:
-                            myToDos.PrintToDos();
-                            Console.WriteLine("\nEnter the number of the task you would like to mark as completed:");
-                            currentInput = Console.ReadLine();
-                            if (int.TryParse(currentInput, out currentToDo))
+                            if (CheckNoOfToDos())
                             {
-                                ValidateInput(myToDos, currentInput, ref currentToDo);
-                                myToDos.CompleteToDo(currentToDo - 1);
-                                Console.WriteLine($"\nTask #{currentToDo} marked as 'Complete'.\n");
-                            }
-                            else
-                            {
-                                Console.WriteLine("\nInvalid selection! Please enter a number.\n");
+                                myToDos.PrintToDos();
+                                Console.WriteLine("\nEnter the number of the task you would like to mark as completed:");
+                                do
+                                { GetInput(MarkAsComplete); }
+                                while (!callbackExecuted);
+                                break;
                             }
                             break;
                         case 3:
-                            myToDos.PrintToDos();
-                            Console.WriteLine("\nEnter the number of the task you would like to update:");
-                            currentInput = Console.ReadLine();
-                            if (int.TryParse(currentInput, out currentToDo))
+                            if (CheckNoOfToDos())
                             {
-                                ValidateInput(myToDos, currentInput, ref currentToDo);
-                                Console.WriteLine("Enter the new task:");
-                                newTask = Console.ReadLine();
-                                myToDos.UpdateTask(newTask, currentToDo - 1);
-                                Console.WriteLine($"\nTask #{currentToDo} updated.\n");
-                            }
-                            else
-                            {
-                                Console.WriteLine("\nInvalid selection! Please enter a number.\n");
+                                myToDos.PrintToDos();
+                                Console.WriteLine("\nEnter the number of the task you would like to update:");
+                                do
+                                { GetInput(UpdateToDo); }
+                                while (!callbackExecuted);
+                                break;
                             }
                             break;
                         case 4:
                             myToDos.PrintToDos();
                             break;
                         case 5:
-                            myToDos.PrintToDos();
-                            Console.WriteLine("\nEnter the number of the task you would like to delete:");
-                            currentInput = Console.ReadLine();
-                            if (int.TryParse(currentInput, out currentToDo))
+                            if (CheckNoOfToDos())
                             {
-                                ValidateInput(myToDos, currentInput, ref currentToDo);
-                                myToDos.RemoveToDo(currentToDo - 1);
-                                Console.WriteLine($"\nTask #{currentToDo} deleted.\n");
-                            }
-                            else
-                            {
-                                Console.WriteLine("\nInvalid selection! Please enter a number.\n");
+                                myToDos.PrintToDos();
+                                Console.WriteLine("\nEnter the number of the task you would like to delete:");
+                                do
+                                { GetInput(DeleteToDo); }
+                                while (!callbackExecuted);
+                                break;
                             }
                             break;
                         case 6:
@@ -108,24 +97,72 @@ namespace dotnet_todo
 
         }
 
-        static bool CheckRange(ToDoList list, int index)
+        // Option methods
+        static void MarkAsComplete()
         {
-            if (index < 1 || index > list.Count())
+            myToDos.CompleteToDo(currentToDo - 1);
+            Console.WriteLine($"\nTask #{currentToDo} marked as 'Complete'.\n");
+        }
+
+        static void UpdateToDo()
+        {
+            Console.WriteLine("Enter the new task:");
+            NewTask = Console.ReadLine();
+            myToDos.UpdateTask(NewTask, currentToDo - 1);
+            Console.WriteLine($"\nTask #{currentToDo} updated.\n");
+        }
+
+        static void DeleteToDo()
+        {
+            myToDos.RemoveToDo(currentToDo - 1);
+            Console.WriteLine($"\nTask #{currentToDo} deleted.\n");
+        }
+
+        // Helper methods
+        static bool CheckNoOfToDos()
+        {
+            if (myToDos.Count() == 0)
             {
-                Console.WriteLine($"\nInvalid selection. Select a number between 1 and {list.Count()}.");
+                Console.WriteLine("Error: You need to create a to-do item to perform this task.");
+                return false;
+            }
+            return true;
+        }
+        static void GetInput(Callback callBack)
+        {
+            callbackExecuted = false;
+            currentInput = Console.ReadLine();
+
+            if (int.TryParse(currentInput, out currentToDo))
+            {
+                ValidateInput();
+                callBack();
+                callbackExecuted = true;
+            }
+            else
+            {
+                Console.WriteLine("\nInvalid selection! Please enter a number:");
+            }
+        }
+
+        static bool CheckRange()
+        {
+            if (currentToDo < 1 || currentToDo > myToDos.Count())
+            {
+                Console.WriteLine($"\nInvalid selection. Select a number between 1 and {myToDos.Count()}.");
                 return true;
             }
             return false;
         }
 
-        static void ValidateInput(ToDoList list, String input, ref int index)
+        static void ValidateInput()
         {
-            while (CheckRange(list, index))
+            while (CheckRange())
             {
-                input = Console.ReadLine();
-                if (int.TryParse(input, out index))
+                currentInput = Console.ReadLine();
+                if (int.TryParse(currentInput, out currentToDo))
                 {
-                    CheckRange(list, index);
+                    CheckRange();
                 }
                 else
                 {

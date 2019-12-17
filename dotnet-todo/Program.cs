b468b129698ff448cc472e6d19633525e8197ea7
Glen Carpenter <glen@glencarpenter.io>
2019-12-17 09:57:14 -0700

    exception handling for invalid input

--- dotnet-todo/Program.cs
+++ dotnet-todo/Program.cs
@@ -8,7 +8,8 @@ namespace dotnet_todo
         {
             // initializing todo list
             ToDoList myToDos = new ToDoList();
-            int selection = 0;
+            String input;
+            int selection;
 
             // vars for current selection
             String newTask;
@@ -27,48 +28,53 @@ namespace dotnet_todo
                 Console.WriteLine("5. Delete a to-do item.");
                 Console.WriteLine("6. Quit.");
 
-                selection = Convert.ToInt32(Console.ReadLine());
-
-                switch (selection)
+                input = Console.ReadLine();
+                if (int.TryParse(input, out selection))
                 {
-                    case 1:
-                        Console.WriteLine("\nEnter the new to-do item: ");
-                        newTask = Console.ReadLine();
-                        myToDos.AddToDo(newTask);
-                        Console.WriteLine("\nNew item added.\n");
-                        break;
-                    case 2:
-                        myToDos.PrintToDos();
-                        Console.WriteLine("\nEnter the number of the task you would like to mark as completed:");
-                        currentToDo = Convert.ToInt32(Console.ReadLine()) - 1;
-                        myToDos.CompleteToDo(currentToDo);
-                        Console.WriteLine($"\nTask #{currentToDo + 1} marked as 'Complete'.\n");
-                        break;
-                    case 3:
-                        myToDos.PrintToDos();
-                        Console.WriteLine("\nEnter the number of the task you would like to update:");
-                        currentToDo = Convert.ToInt32(Console.ReadLine()) - 1;
-                        Console.WriteLine("Enter the new task:");
-                        newTask = Console.ReadLine();
-                        myToDos.UpdateTask(newTask, currentToDo);
-                        Console.WriteLine($"\nTask #{currentToDo + 1} updated.\n");
-                        break;
-                    case 4:
-                        myToDos.PrintToDos();
-                        break;
-                    case 5:
-                        myToDos.PrintToDos();
-                        Console.WriteLine("\nEnter the number of the task you would like to delete:");
-                        currentToDo = Convert.ToInt32(Console.ReadLine()) - 1;
-                        myToDos.RemoveToDo(currentToDo);
-                        Console.WriteLine($"\nTask #{currentToDo + 1} deleted.\n");
-                        break;
-                    case 6:
-                        Console.WriteLine("\nGood bye!\n");
-                        break;
-                    default:
-                        Console.WriteLine("\nInvalid selection!\n");
-                        break;
+                    switch (selection)
+                    {
+                        case 1:
+                            Console.WriteLine("\nEnter the new to-do item: ");
+                            newTask = Console.ReadLine();
+                            myToDos.AddToDo(newTask);
+                            Console.WriteLine("\nNew item added.\n");
+                            break;
+                        case 2:
+                            myToDos.PrintToDos();
+                            Console.WriteLine("\nEnter the number of the task you would like to mark as completed:");
+                            currentToDo = Convert.ToInt32(Console.ReadLine()) - 1;
+                            myToDos.CompleteToDo(currentToDo);
+                            Console.WriteLine($"\nTask #{currentToDo + 1} marked as 'Complete'.\n");
+                            break;
+                        case 3:
+                            myToDos.PrintToDos();
+                            Console.WriteLine("\nEnter the number of the task you would like to update:");
+                            currentToDo = Convert.ToInt32(Console.ReadLine()) - 1;
+                            Console.WriteLine("Enter the new task:");
+                            newTask = Console.ReadLine();
+                            myToDos.UpdateTask(newTask, currentToDo);
+                            Console.WriteLine($"\nTask #{currentToDo + 1} updated.\n");
+                            break;
+                        case 4:
+                            myToDos.PrintToDos();
+                            break;
+                        case 5:
+                            myToDos.PrintToDos();
+                            Console.WriteLine("\nEnter the number of the task you would like to delete:");
+                            currentToDo = Convert.ToInt32(Console.ReadLine()) - 1;
+                            myToDos.RemoveToDo(currentToDo);
+                            Console.WriteLine($"\nTask #{currentToDo + 1} deleted.\n");
+                            break;
+                        case 6:
+                            Console.WriteLine("\nGood bye!\n");
+                            break;
+                        default:
+                            Console.WriteLine("\nInvalid selection!\n");
+                            break;
+                    }
+                }
+                else{
+                    Console.WriteLine("\nInvalid selection! Please enter a number.\n");
                 }
 
             }
